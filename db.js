17a3cea7fb3b8f8b0133fe860792a92ce0d890bf
ninjpandaX <gkz1256@outlook.com>
2023-08-26 15:47:54 +0530

    resolve

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const mongoUrl = process.env.database;
-
+mongoose.set('strictQuery', true);
 const connectToMongo = ()=>{
     mongoose.connect(mongoUrl,()=>{
         console.log('connected to Mongo hello successfully');
